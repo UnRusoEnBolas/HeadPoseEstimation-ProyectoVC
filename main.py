00c1721797f98fe0259cb5bf81fbbf2b19f98b3a
UnRusoEnBolas <jgimenezp99@gmail.com>
2021-05-18 19:54:22 +0200

    .

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 import numpy as np
 import cv2
-import dlib
+import mediapipe as mp
 from imutils.video import FPS
-import faceDetection as fd
-import landmarkLocalization as ll
-import poseEstimation as pe
 
-# Más bajo -< más rápido, peor detección
-# Más alto -> más lento, mejor detección
-# No bajar de 0 (no incluído) y no recomendable pasar de 1
-DOWNSIZE_FACTOR = 0.5
-FACE_DETECTION_MODEL = "./trainedModels/haarcascade_frontalface_default.xml"
-SHAPE_MODEL = "./trainedModels/shape_predictor_68_face_landmarks.dat"
-
-faceDetector = cv2.CascadeClassifier(FACE_DETECTION_MODEL)
-shapePredictor = dlib.shape_predictor(SHAPE_MODEL)
+mediaPipeFaceMesh = mp.solutions.face_mesh
 videoInput = cv2.VideoCapture(0)
 
 ret, frame = videoInput.read()
@@ -22,49 +11,49 @@ size = frame.shape
 focal_length = size[1]
 center = (size[1]/2, size[0]/2)
 cameraMat = np.array(
-                        [[focal_length, 0, center[0]],
-                        [0, focal_length, center[1]],
-                        [0, 0, 1]], dtype = np.float64
-                        )
+    [[focal_length, 0, center[0]],
+     [0, focal_length, center[1]],
+     [0, 0, 1]], dtype=np.float64
+)
 
 # Modelo 3D estándar
 modelPoints = np.array([
-                            (0.0, 0.0, 0.0),             # Punta nariz
-                            (0.0, -330.0, -65.0),        # Barbilla
-                            (-225.0, 170.0, -135.0),     # Ojo izquierdo izquierda
-                            (225.0, 170.0, -135.0),      # Ojo derecho derecha
-                            (-150.0, -150.0, -125.0),    # Boca izquierda
-                            (150.0, -150.0, -125.0)      # Boca derecha
-                        ])
+    (0.0, 0.0, 0.0),             # Punta nariz
+    (0.0, -330.0, -65.0),        # Barbilla
+    # Ojo izquierdo izquierda
+    (-225.0, 170.0, -135.0),
+    (225.0, 170.0, -135.0),      # Ojo derecho derecha
+    (-150.0, -150.0, -125.0),    # Boca izquierda
+    (150.0, -150.0, -125.0)      # Boca derecha
+])
 
 # Comenzar contador de FPS
 fps = FPS().start()
 
-while True:
-    ret, frame = videoInput.read()
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    height, width = grayFrame.shape
-    height = int(height*DOWNSIZE_FACTOR)
-    width = int(width*DOWNSIZE_FACTOR)
-    # Reducimos (o no) el tamaño.
-    resizedGrayFrame = cv2.resize(grayFrame, (int(width), int(height)))
+with mediaPipeFaceMesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5) as faceMesh:
+    while True:
+        ret, frame = videoInput.read()
+        rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    detected, box = fd.getBoundingBox(faceDetector, resizedGrayFrame)
-    if detected:
-        # Por si se ha hecho downsizing, se recupera el tamaño del la bb
-        drawingBox = (box/DOWNSIZE_FACTOR).astype(np.int64)
-        (x,y,w,h) = drawingBox       
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)    
-        
-        # Obtenemos 68 puntos de referencia de la cara detectada 
-        landmarksCoords = ll.getLandmarksCoordinates(shapePredictor, resizedGrayFrame, box)
-        drawingCoords = (landmarksCoords/DOWNSIZE_FACTOR).astype(np.int64)  
-        for idx, coord in enumerate(drawingCoords):
-            if idx in [33,8,36,45,48,54]:
-                cv2.circle(frame, coord, 3, (0,255,255))
-            else:
-                cv2.circle(frame, coord, 1, (0,255,0))
+        # ------------------------------TESTING----------------------------------
+        rgbFrame.flags.writeable = False
+        res = faceMesh.process(rgbFrame)
+        rgbFrame.flags.writeable = True
 
+        landmarksCoords = np.empty((468, 2), dtype=np.float64)
+        if res.multi_face_landmarks:
+            for detectedLandmarks in res.multi_face_landmarks:
+                for i in range(0, 468):
+                    landmarksCoords[i] = [
+                        detectedLandmarks.landmark[i].x,
+                        detectedLandmarks.landmark[i].y
+                    ]
+            landmarksCoords = landmarksCoords * np.array([[size[1], size[0]]])
+            for landmarkCoord in landmarksCoords.astype(np.int64):
+                cv2.circle(frame, landmarkCoord, 1, (0,255,0))
+        # -----------------------------------------------------------------------
+
+        '''
         # Obtenemos la estimation pose
 
         # Preparamos los puntos claves de los landmarks
@@ -86,20 +75,21 @@ while True:
         p1 = (int(poseEstimationLandmarks[0][0]),int(poseEstimationLandmarks[0][1]))
         p2 = (int(noseTip[0][0][0]/DOWNSIZE_FACTOR), int(noseTip[0][0][1]/DOWNSIZE_FACTOR))
         cv2.line(frame, p1, p2, (0,0,255), 2)
-        
-         
-    cv2.imshow('Head pose estimation by Juan Carlos Soriano and Jorge Gimenez', frame)
+        '''
 
-    # Actualizar el contador de FPS
-    fps.update()
-    
-    # 27 es el código de ESC en mi MacBook Pro (puede cambiar para cada PC)
-    if cv2.waitKey(1) == 27:
-        break
+        cv2.imshow(
+            'Head pose estimation by Juan Carlos Soriano and Jorge Gimenez', frame)
+
+        # Actualizar el contador de FPS
+        fps.update()
+
+        # 27 es el código de ESC en mi MacBook Pro (puede cambiar para cada PC)
+        if cv2.waitKey(1) == 27:
+            break
 
 # Informe de los FPS
 fps.stop()
-print("[INFO] tiempo: {:.2f}".format(fps.elapsed()),"s")
+print("[INFO] tiempo: {:.2f}".format(fps.elapsed()), "s")
 print("[INFO] FPS: {:.2f}".format(fps.fps()))
 
 videoInput.release()
