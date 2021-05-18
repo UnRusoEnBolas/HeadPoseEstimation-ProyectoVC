7d1d130e9f187bdf59dfa79ee40237a51ff6a1e3
Juan Carlos Soriano Valle <thelakegray@gmail.com>
2021-05-18 12:04:12 +0200

    esta malito

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from utils import get_child_subgraph_dpu
 # Más bajo -< más rápido, peor detección
 # Más alto -> más lento, mejor detección
 # No bajar de 0 (no incluído) y no recomendable pasar de 1
-DOWNSIZE_FACTOR = 0.5
+DOWNSIZE_FACTOR = 1
 FACE_DETECTION_MODEL = "./trainedModels/haarcascade_frontalface_default.xml"
 SHAPE_MODEL = "./trainedModels/shape_predictor_68_face_landmarks.dat"
 
@@ -79,7 +79,26 @@ while True:
                 cv2.circle(frame, coord, 1, (0,255,0))
 
             # Obtenemos la estimation pose
-            
+
+            # Preparamos los puntos claves de los landmarks
+            image_points = np.array([
+                        (landmarksCoords[33,0], landmarksCoords[33,1]), # Nariz
+                        (landmarksCoords[8,0], landmarksCoords[8,1]), # Barbilla
+                        (landmarksCoords[36,0], landmarksCoords[36,1]), # Extremo ojo izquierdo
+                        (landmarksCoords[45,0], landmarksCoords[45,1]), # Extremo ojo derecho
+                        (landmarksCoords[48,0], landmarksCoords[48,1]), # Extremo izquierdo boca
+                        (landmarksCoords[54,0], landmarksCoords[54,1])  # Extremo derecho boca
+            ], dtype="float")
+
+            success, rotation_vector, translation_vector = pe.poseEstimation(model_points, camera_matrix, image_points)
+            image_points = image_points / DOWNSIZE_FACTOR
+            (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, np.zeros((4,1)))
+            print(nose_end_point2D)
+             # Dibujamos la linea de la pose
+            p1 = (int(image_points[0][0]),int(image_points[0][1]))
+            p2 = (int(nose_end_point2D[0][0][0]/DOWNSIZE_FACTOR), int(nose_end_point2D[0][0][1]/DOWNSIZE_FACTOR))
+            cv2.line(frame, p1, p2, (0,0,255), 2)
+            #cv2.line(frame, (int(image_points[0][0]),int(image_points[0][1])), (int(image_points[1][0]),int(image_points[1][1])), (255,0,0), 2)
          
     cv2.imshow('Head pose estimation by Juan Carlos Soriano and Jorge Gimenez', frame)
 
--- poseEstimation.py
+++ poseEstimation.py
@@ -7,30 +7,18 @@ detThreshold = 0.55
 nmsThreshold = 0.35
                      
 
-def poseEstimation(frame, face, drawingBox, landmarks):
+def poseEstimation(model_points, camera_matrix, image_points):
 
-    (x,y,w,h) = drawingBox
-    widthX = x+w
-    heightY = y+h
+    return cv2.solvePnP(model_points, image_points, camera_matrix, np.zeros((4,1)), flags=cv2.SOLVEPNP_ITERATIVE)
+    """return (success, rotation_vector, translation_vector)
+    (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+
+    # Dibujamos la linea de la pose
+    p1 = ( widthX+int(image_points[0][0]), heightY+int(image_points[0][1]))
+    p2 = ( widthX+int(nose_end_point2D[0][0][0]), heightY+int(nose_end_point2D[0][0][1]))"""
     
 
-    # Preparamos los puntos claves de los landmarks
-    image_points = np.array([
-                (landmarks[33,0]*widthX, landmarks[33,1]*heightY), # Nariz
-                (landmarks[8,0]*widthX, landmarks[8,1]*heightY), # Barbilla
-                (landmarks[36,0]*widthX, landmarks[36,1]*heightY), # Extremo ojo izquierdo
-                (landmarks[45,0]*widthX, landmarks[45,1]*heightY), # Extremo ojo derecho
-                (landmarks[48,0]*widthX, landmarks[48,1]*heightY), # Extremo izquierdo boca
-                (landmarks[54,0]*widthX, landmarks[54,1]*heightY)  # Extremo derecho boca
-    ], dtype="double")
 
-    # Estimamos posiciones de las puntos característicos
-    eye_center_x = (image_points[2][0]+image_points[3][0])/2
-    eye_center_y = (image_points[2][1]+image_points[3][1])/2
-    nose_offset_x = (image_points[0][0] - eye_center_x)
-    nose_offset_y = (image_points[0][1] - eye_center_y)
-    mouth_center_x = (image_points[4][0] + image_points[5][0])/2
-    mouth_center_y = (image_points[4][1] + image_points[5][1])/2
 
     
 
